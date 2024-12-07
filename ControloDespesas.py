c357d81ed17b12f4e4b1e720d6bb45800a7f87ff
Azeved0 <86888359+Azeved0@users.noreply.github.com>
2024-12-07 20:59:54 +0000

    Update ControloDespesas.py
    new token

--- ControloDespesas.py
+++ ControloDespesas.py
@@ -11,7 +11,7 @@ st.set_page_config(layout="wide")
 repo = "Azeved0/Controlo-de-Gastos"  # Your repo name
 file_path = "Despesas.csv"  # Path to the file in the repository
 branch = "main"  # Branch name
-token = "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"
+token = "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"
 
 # URL of the raw CSV file
 #url = f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/{file_path}"
