e04fdd2cbe7d3f2cea9f7155705ac73c2ac8eaee
Azeved0 <86888359+Azeved0@users.noreply.github.com>
2024-12-09 02:01:12 +0000

    Update ControloDespesas.py
    import json

--- ControloDespesas.py
+++ ControloDespesas.py
@@ -3,6 +3,7 @@ import streamlit as st
 from datetime import datetime
 from google.oauth2.service_account import Credentials
 import gspread
+import json
 
 # Google Sheets Setup
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
