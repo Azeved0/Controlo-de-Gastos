3ac89259a6df64a9542f77f7bc76ff79f8ab70a0
Azeved0 <86888359+Azeved0@users.noreply.github.com>
2024-12-09 02:03:44 +0000

    Update ControloDespesas.py
    update

--- ControloDespesas.py
+++ ControloDespesas.py
@@ -3,15 +3,15 @@ import streamlit as st
 from datetime import datetime
 from google.oauth2.service_account import Credentials
 import gspread
-import json
 
 # Google Sheets Setup
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 SPREADSHEET_ID = "1ZssUo6wfy4wZO9eQPljOTdwzF7dTSpxZ1_wNa6yGqaw"  # Replace with your Google Sheets ID
 
-# Load credentials from secrets
-credentials_dict = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
-credentials = Credentials.from_service_account_info(credentials_dict, scopes=SCOPES)
+# Load credentials from Streamlit secrets
+credentials = Credentials.from_service_account_info(st.secrets["GOOGLE_CREDENTIALS"], scopes=SCOPES)
+
+# Authenticate with Google Sheets
 client = gspread.authorize(credentials)
 sheet = client.open_by_key(SPREADSHEET_ID).sheet1
 
